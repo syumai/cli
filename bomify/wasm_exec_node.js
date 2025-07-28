1f9cc39509af9707390ba93d45257fde253a3426
syumai <syumai@gmail.com>
2025-07-28 17:49:24 +0900

    modified wasm_exec_node.js

--- bomify/wasm_exec_node.js
+++ bomify/wasm_exec_node.js
@@ -4,11 +4,6 @@
 
 "use strict";
 
-if (process.argv.length < 3) {
-	console.error("usage: go_js_wasm_exec [wasm binary] [arguments]");
-	process.exit(1);
-}
-
 globalThis.require = require;
 globalThis.fs = require("fs");
 globalThis.path = require("path");
@@ -22,19 +17,23 @@ globalThis.crypto ??= require("crypto");
 require("./wasm_exec");
 
 const go = new Go();
-go.argv = process.argv.slice(2);
+go.argv = process.argv.slice(1);
 go.env = Object.assign({ TMPDIR: require("os").tmpdir() }, process.env);
 go.exit = process.exit;
-WebAssembly.instantiate(fs.readFileSync(process.argv[2]), go.importObject).then((result) => {
-	process.on("exit", (code) => { // Node.js exits if no event handler is pending
-		if (code === 0 && !go.exited) {
-			// deadlock, make Go print error and stack traces
-			go._pendingEvent = { id: 0 };
-			go._resume();
-		}
-	});
-	return go.run(result.instance);
-}).catch((err) => {
-	console.error(err);
-	process.exit(1);
-});
+const wasmPath = path.resolve(__dirname, "./main.wasm");
+WebAssembly.instantiate(fs.readFileSync(wasmPath), go.importObject)
+  .then((result) => {
+    process.on("exit", (code) => {
+      // Node.js exits if no event handler is pending
+      if (code === 0 && !go.exited) {
+        // deadlock, make Go print error and stack traces
+        go._pendingEvent = { id: 0 };
+        go._resume();
+      }
+    });
+    return go.run(result.instance);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
